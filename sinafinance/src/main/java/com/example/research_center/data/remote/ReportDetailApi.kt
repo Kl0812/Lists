02d7f0c6fb256d0c5265e421a8ed0271be55a984
Kl0812 <1543913121@qq.com>
2025-01-09 17:40:46 +0800

    Add page switching animation

--- sinafinance/src/main/java/com/example/research_center/data/remote/ReportDetailApi.kt
+++ sinafinance/src/main/java/com/example/research_center/data/remote/ReportDetailApi.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 interface ReportDetailApi {
 
     // Get report detail information
-    @GET("report/api/openapi.php/ReportService.getShow?")
+    @GET("stock/api/openapi.php/ReportService.getShow?")
     suspend fun getShowById(
         @Query("fromsrc") fromsrc: String = "app", // 不知道是什么参数，但影响"reportinfo"排版的换行
         @Query("rptid") rptid: String // 研报ID，必须传参
--- sinafinance/src/main/java/com/example/research_center/presentation/navigation.kt
+++ sinafinance/src/main/java/com/example/research_center/presentation/navigation.kt
@@ -11,8 +11,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.research_center.presentation.qs_symbol_list.QsSymbolListScreen
-import com.example.research_center.presentation.research_center.ResearchCenterScreen
 import com.example.research_center.presentation.report_detail.ReportDetailScreen
+import com.example.research_center.presentation.research_center.ResearchCenterScreen
 import com.example.research_center.presentation.stock.StockScreen
 
 @Composable
@@ -22,6 +22,31 @@ fun AppNavHost(
     NavHost(
         navController = navController,
         startDestination = Screen.ResearchCenterScreen.route,
+        // Move forward
+        enterTransition = {
+            // 新页面从右侧进入 => 向左方向滑入
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(300, easing = EaseIn)
+            )
+        },
+        exitTransition = {
+            // 旧页面保持不动
+            ExitTransition.None
+        },
+        // Move back
+        popEnterTransition = {
+            // 新页面保持不动
+            EnterTransition.None
+        },
+        popExitTransition = {
+            // 当前页面向右退出 => 向右方向滑出
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(300, easing = EaseOut)
+            )
+        }
+
     ) {
         composable(
             route = Screen.ResearchCenterScreen.route,
